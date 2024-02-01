18f41b14bdbd5030f9a555b05c3c5c87d1e61214
Himanshu <himanshusingh3179@gmail.com>
2024-02-01 10:29:34 +0530

    redirect to /todos

--- app/page.tsx
+++ app/page.tsx
@@ -1,12 +1,14 @@
 import { Button } from '@radix-ui/themes'
 import Link from 'next/link'
+import { redirect } from 'next/navigation'
 
 export default function Home() {
   return (
-    <main className='h-screen flex justify-center items-center'>
-      <Link href={'/todos'}>
-        <Button size={"3"}>Todos</Button>
-      </Link>
-    </main>
+    // <main className='h-screen flex justify-center items-center'>
+    //   <Link href={'/todos'}>
+    //     <Button size={"3"}>Todos</Button>
+    //   </Link>
+    // </main>
+    redirect('/todos')
   )
 }
